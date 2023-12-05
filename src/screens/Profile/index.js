174eb9d436a87736546ffdb1fb8a74e3883d77c1
rzqthya <athiyya.fan178@gmail.com>
2023-12-05 13:40:02 +0700

    +firebase setup

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,18 +1,90 @@
 import React, { useState, useEffect } from "react";
 import { Box, Text, Image, VStack, ScrollView } from "@gluestack-ui/themed";
-import { Button } from "../../components/kecil";
+import { Button } from "../../components";
+import { clearStorage, getData } from "../../utils";
+import FIREBASE from "../../config/FIREBASE/INDEX.JS";
 
 const Profile = ({ navigation }) => {
+    const [profile, setProfile] = useState(null);
+
+    const getUserData = () => {
+        getData("user").then((res) => {
+            const data = res;
+            if (data) {
+                console.log("isi data", data);
+                setProfile(data);
+            } else {
+                // navigation.replace('Login');
+            }
+        });
+    };
+
+    useEffect(() => {
+        const unsubscribe = navigation.addListener("focus", () => {
+            getUserData();
+        });
+
+        return () => {
+            unsubscribe();
+        };
+    }, [navigation]);
+
+    const onSubmit = (profile) => {
+        if (profile) {
+            FIREBASE.auth()
+                .signOut()
+                .then(() => {
+                    // Sign-out successful.
+                    clearStorage();
+                    navigation.replace("MainApp");
+                })
+                .catch((error) => {
+                    // An error happened.
+                    alert(error);
+                });
+        } else {
+            navigation.replace("Login");
+        }
+    };
+
     return (
-        <Box mt={"$5"} mx={"$5"} backgroundColor="$blueGray100" flex={1} marginTop={"$20"} flexDirection="column">
+        <Box
+            mt={"$5"}
+            mx={"$5"}
+            backgroundColor="$blueGray100"
+            flex={1}
+            marginTop={"$20"}
+            flexDirection="column"
+        >
             <ScrollView>
                 <VStack backgroundColor="$blueGray100" width={"$full"} mb={"$10"}>
-                    <Image source={require("../../assets/images/avatar.png")} size="2xl" borderRadius={"$full"} alignSelf="center" alt="Foto Profil" />
-                    <Text fontSize={"$xl"} alignSelf="center" marginTop={"$5"} fontWeight="$bold">
-                        Nama User
+                    <Image
+                        source={require("../../assets/images/avatar.png")}
+                        size="2xl"
+                        borderRadius={"$full"}
+                        alignSelf="center"
+                        alt="Foto Profil"
+                    />
+                    <Text
+                        fontSize={"$xl"}
+                        alignSelf="center"
+                        marginTop={"$5"}
+                        fontWeight="$bold"
+                    >
+                        {profile?.nama}
                     </Text>
                 </VStack>
-                <Box flexDirection="column" bgColor="$white" shadowColor="$black" shadowOffset={{ width: 0, height: 2 }} shadowOpacity={"$25"} shadowRadius={"$3.5"} justifyContent="space-evenly" p={"$5"} borderRadius={"$xl"}>
+                <Box
+                    flexDirection="column"
+                    bgColor="$white"
+                    shadowColor="$black"
+                    shadowOffset={{ width: 0, height: 2 }}
+                    shadowOpacity={"$25"}
+                    shadowRadius={"$3.5"}
+                    justifyContent="space-evenly"
+                    p={"$5"}
+                    borderRadius={"$xl"}
+                >
                     <Text color="$black" fontWeight="$bold" fontSize={"$xl"}>
                         Data Diri
                     </Text>
@@ -21,7 +93,7 @@ const Profile = ({ navigation }) => {
                             Email
                         </Text>
                         <Text color="$black" fontSize={"$xl"} mt={"$2"}>
-                            Lorem Ipsum
+                            {profile?.email}
                         </Text>
                     </Box>
                     <Box mt={"$5"}>
@@ -29,17 +101,15 @@ const Profile = ({ navigation }) => {
                             Nomor Ponsel
                         </Text>
                         <Text color="$black" fontSize={"$xl"} mt={"$2"}>
-                            Lorem Ipsum
+                            {profile?.nohp}
                         </Text>
                     </Box>
                 </Box>
                 <Button
                     type="text"
-                    title={"Login"}
+                    title={profile ? "Logout" : "Login"}
                     padding={"$3"}
-                    onPress={() => {
-                        navigation.navigate("Login");
-                    }}
+                    onPress={() => onSubmit(profile)}
                 />
             </ScrollView>
         </Box>
--- src/screens/Splash/index.js
+++ src/screens/Splash/index.js
@@ -1,11 +1,17 @@
-import { StyleSheet, View } from "react-native";
-import React, { Component } from "react";
-import { IconLogo } from "../../assets";
+import { StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+import { IconLogo } from '../../assets'
+import { getData } from '../../utils/localStorage/INDEX.JS'
 
 export default class Splash extends Component {
     componentDidMount() {
-        setTimeout(() => {
-            this.props.navigation.navigate('Login');
+        setTimeout(async () => {
+            const userData = await getData("user");
+            if (userData) {
+                this.props.navigation.replace('MainApp');
+            } else {
+                this.props.navigation.navigate('Login');
+            }
         }, 3000)
     }
 
@@ -14,14 +20,14 @@ export default class Splash extends Component {
             <View style={styles.pages}>
                 <IconLogo />
             </View>
-        );
+        )
     }
 }
 
 const styles = StyleSheet.create({
     pages: {
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
